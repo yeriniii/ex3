07b4c7d79626ec209c1f8c1ae0d98d7f0b176c66
yeriniii <yerini000303@naver.com>
2024-02-07 00:45:59 +0900

    ex3

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -2,14 +2,29 @@ import React from "react";
 import { DropdownProvider, useDropdown } from "./DropdownContext";
 
 const DropdownButton = () => {
-  return <button>Dropdown Button</button>;
+  const { toggleDropdown, selectedItem } = useDropdown();
+  return (
+    <button onClick={() => toggleDropdown()}>
+      {selectedItem === null ? "Dropdown Button" : selectedItem}
+    </button>
+  );
 };
 
 const DropdownMenu = ({ items }) => {
+  const { isOpen, selectItem } = useDropdown();
+  if (!isOpen) {
+    return null;
+  }
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li
+          style={{ cursor: "pointer" }}
+          key={item}
+          onClick={() => selectItem(item)}
+        >
+          {item}
+        </li>
       ))}
     </ul>
   );
